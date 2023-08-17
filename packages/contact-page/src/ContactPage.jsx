a4af6b280b0a201b93dbdfb47dd1ec3eb27e91de
Ethan Domingo <etddomin@ucsc.edu>
2023-08-17 12:27:42 -0700

    Added contact-page display

--- packages/contact-page/src/ContactPage.jsx
+++ packages/contact-page/src/ContactPage.jsx
@@ -1,4 +1,14 @@
 import React from "react";
+import { EmailJSResponseStatus } from "emailjs-com";
+import {
+  Box,
+  Typography,
+  Grid,
+  Box,
+  Fade,
+  Container,
+  IconButton,
+} from "@mui/material";
 
 export const ContactPage = () => {
   return <div>Contact Page</div>;
