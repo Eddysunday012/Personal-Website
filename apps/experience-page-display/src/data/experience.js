4cf3549bcb85de3d6c7a6fa1d43f8c54b3f4bc47
Ethan Domingo <etddomin@ucsc.edu>
2024-04-15 20:33:17 -0700

    finished experience page update

--- apps/experience-page-display/src/data/experience.js
+++ apps/experience-page-display/src/data/experience.js
@@ -3,20 +3,32 @@ import caltransLogo from "../assets/caltransLogo.png";
 
 export const experience = [
   {
-    name: "CalTrans",
+    name: "Student Assistant Programmer",
+    dates: "July 2021 - July 2023",
     years: 2,
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "For 2 years, I served as a remote Student Assistant for the Division 7 CalTrans design engineering team.",
+    tasks: [
+      "Implemented automation scripts in Microsoft’s VBA programming language, that streamlined processes up by 50% ",
+      "Quantity checked Senior Engineer’s Excel Sheets and uploaded them to a SharePoint file server for other Engineers to utilize",
+      "Redesigned a database file storage system using Microsoft Access and Microsoft VBA",
+    ],
     skills: ["Python", "Excel", "Access", "SharePoint", "VBA", "CADD"],
     logo: {
       src: caltransLogo,
     },
   },
   {
-    name: "UC Observatory",
+    name: "Undergraduate Researcher",
     years: 0.5,
+    dates: "June 2022 - Sept 2022",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "For the summer of 2022, I served as an Undergraduate Research Assistant for the UC Observatory, specifically under a team researching Fast Radio Bursts, or FRBs for short.",
+    tasks: [
+      "Contributed to a Python-based Github Respository used by Astrophysics Researchers to analyze telescope data",
+      "Created a comprehensive time-profiling analysis to identify and optimize bottlenecks within the main codebase",
+      "Contributions resulted in an a 5x speedup and saving 8 hours in the overall data processing time performed by the code",
+    ],
     skills: ["Python", "Tensorflow", "Data Analytics", "Numpy"],
     logo: {
       src: ucoLogo,
--- packages/experience-page/src/ExperiencePage.jsx
+++ packages/experience-page/src/ExperiencePage.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { DependenciesContext } from "dependenciescontext";
-import { Grid, Box, Container, Typography } from "@mui/material";
+import { Grid, Box, Container, Typography, Avatar } from "@mui/material";
 
 export const ExperiencePage = () => {
   const ImageMyStyle = {
-    maxHeight: "130px",
+    maxHeight: "80px",
     flex: 1,
     resizeMode: "contain",
     margin: 20,
   };
   const ExperienceBox = ({ exp }) => {
     return (
-      <Grid item xs={8} md={4} style={{ textAlign: "center" }} sx={{ m: 2 }}>
+      <Grid item xs={12} md={5.8} style={{ textAlign: "center" }} sx={{ m: 1 }}>
         <Box
           display="flex-start"
           alignItems="stretch"
@@ -30,36 +30,70 @@ export const ExperiencePage = () => {
             }}
           >
             <img style={ImageMyStyle} src={exp.logo.src} alt="ExpLogo" />
-            <Typography variant="body1" sx={{ mb: 2, color: "#EEEEEE" }}>
+            <Typography
+              variant="h6"
+              sx={{ color: "#EEEEEE", fontWeight: "bold" }}
+            >
               {exp.name}
+            </Typography>
+            <Typography
+              variant="body2"
+              sx={{ padding: 0, color: "#EEEEEE", fontWeight: "bold" }}
+            >
+              {exp.dates}
             </Typography>
             {/* <Typography */}
             {/*   variant="body2" */}
-            {/*   style={{ textAlign: "left", color: "#EEEEEE" }} */}
-            {/*   sx={{ pb: 1 }} */}
+            {/*   style={{ textAlign: "center", color: "#EEEEEE" }} */}
+            {/*   sx={{ pb: 2 }} */}
             {/* > */}
             {/*   {exp.description} */}
             {/* </Typography> */}
-            {/* <Container> */}
-            {/*   <Typography */}
-            {/*     sx={{ */}
-            {/*       wordBreak: "break-word", */}
-            {/*       pb: 1, */}
-            {/*       fontWeight: "bold", */}
-            {/*       color: "#EEEEEE", */}
-            {/*     }} */}
-            {/*     align="center" */}
-            {/*     variant="subtitle2" */}
-            {/*   > */}
-            {/*     {exp.skills.map((skill, index) => { */}
-            {/*       if (index != exp.skills.length - 1) { */}
-            {/*         return skill + ", "; */}
-            {/*       } else { */}
-            {/*         return skill; */}
-            {/*       } */}
-            {/*     })} */}
-            {/*   </Typography> */}
-            {/* </Container> */}
+
+            <Container sx={{ p: 2 }}>
+              {/* <Typography */}
+              {/*   variant="body1" */}
+              {/*   sx={{ */}
+              {/*     textAlign: "left", */}
+              {/*     pb: 1, */}
+              {/*     color: "#EEEEEE", */}
+              {/*     fontWeight: "bold", */}
+              {/*   }} */}
+              {/* > */}
+              {/*   Tasks: */}
+              {/* </Typography> */}
+              {exp.tasks.map((task) => {
+                return (
+                  <Typography
+                    variant="body2"
+                    style={{ textAlign: "left", color: "#EEEEEE" }}
+                    sx={{ pb: 2 }}
+                  >
+                    {">"} {task}
+                  </Typography>
+                );
+              })}
+            </Container>
+            <Container>
+              <Typography
+                sx={{
+                  wordBreak: "break-word",
+                  pb: 2,
+                  fontWeight: "bold",
+                  color: "#EEEEEE",
+                }}
+                align="center"
+                variant="subtitle2"
+              >
+                {exp.skills.map((skill, index) => {
+                  if (index != exp.skills.length - 1) {
+                    return skill + ", ";
+                  } else {
+                    return skill;
+                  }
+                })}
+              </Typography>
+            </Container>
           </Container>
         </Box>
       </Grid>
@@ -76,13 +110,13 @@ export const ExperiencePage = () => {
             sx={{ backgroundColor: "#5A5A5A", borderRadius: "12px", p: 1 }}
             alignContent="center"
           >
-            <Typography
-              variant="h5"
-              sx={{ pb: 1, textAlign: "center", color: "#EEEEEE" }}
-            >
-              Experience
-            </Typography>
-            <Grid container spacing={3} justifyContent="center">
+            {/* <Typography */}
+            {/*   variant="h5" */}
+            {/*   sx={{ pb: 1, textAlign: "center", color: "#EEEEEE" }} */}
+            {/* > */}
+            {/*   Experience */}
+            {/* </Typography> */}
+            <Grid container justifyContent="center">
               {experience.map((exp) => (
                 <ExperienceBox exp={exp} key={exp.name} />
               ))}
